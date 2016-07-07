a95439e52d2b43164c0990958cac2d988786948a
Gokhan Tank <gokhan.tank@winning.com.au>
2016-07-07 20:50:05 +1000

    Obstacles - arrayOfObstacles has been added

--- src/js/game/Obstacles.js
+++ src/js/game/Obstacles.js
@@ -9,6 +9,7 @@
         physics: null,
 
         imageWM:null,
+        arrayOfObstacles:null,
 
         isPlaying:true,
         counter:0,
@@ -42,8 +43,66 @@
             this.view.alpha =1;
 
             this.imageWM = new Image();
-            this.imageWM.src= "images/wm.png"
+            this.imageWM.src= "images/wm.png";
 
+            this.arrayOfObstacles = [
+                {
+                    "boxes": [0,0,1,0,0,0,0,0,0],
+                    "xPos": 1200
+                },
+                {
+                    "boxes": [0,0,0,0,1,1,0,0,0],
+                    "xPos": 1800
+                },
+                {
+                    "boxes": [0,1,1,0,0,0,0,0,0],
+                    "xPos": 2400
+                },
+                {
+                    "boxes": [0,0,0,0,1,0,0,0,0],
+                    "xPos": 3000
+                },
+                {
+                    "boxes": [1,1,1,0,0,0,0,0,0],
+                    "xPos": 3600
+                },
+                {
+                    "boxes": [0,0,0,1,1,1,0,0,0],
+                    "xPos": 4000
+                },
+                {
+                    "boxes": [0,1,1,0,,0,0,0,0],
+                    "xPos": 4600
+                },
+                {
+                    "boxes": [0,0,0,0,0,1,1,0,0],
+                    "xPos": 4900
+                },
+                {
+                    "boxes": [1,0,0,1,1,1,0,0,0],
+                    "xPos": 5500
+                },
+                {
+                    "boxes": [0,1,1,1,1,0,0,0,0],
+                    "xPos": 6000
+                },
+                {
+                    "boxes": [0,0,0,0,1,1,1,1,0],
+                    "xPos": 6500
+                },
+                {
+                    "boxes": [1,0,0,0,1,1,1,0,0],
+                    "xPos": 6800
+                },
+                {
+                    "boxes": [0,0,1,1,1,0,0,0,0],
+                    "xPos": 7100
+                },
+                {
+                    "boxes": [1,1,0,0,0,0,1,1,1],
+                    "xPos": 7500
+                }
+            ];
 
             var that = this;
 
@@ -52,6 +111,25 @@
 
                 paint:function(sprite,context){
 
+                    var obj;
+                    var length = that.arrayOfObstacles.length;
+
+                    for (i = 0; i < length; i++) {
+                        obj = that.arrayOfObstacles[i];
+
+                        that.view.local(that.body.x, that.body.y);
+                        console.log(obj.boxes.length);
+
+                        var l = obj.boxes.length;
+                        for (n = 0; n < l; n++) {
+                            if(obj.boxes[n]===1) that.view.drawImage(that.imageWM, obj.xPos, n*51, that.imageWM.width, that.imageWM.height);
+                        }
+                        
+                        that.view.unlocal();
+                    }
+
+
+                    /*
                     if(that.imageWM.complete){
 
                         that.view.local(that.body.x, that.body.y);
@@ -60,6 +138,7 @@
                         that.view.drawImage(that.imageWM, -that.imageWM.width/2, 102, that.imageWM.width, that.imageWM.height);
                         that.view.unlocal();
                     }
+                    */
                     
                 }
 
@@ -89,17 +168,17 @@
 
             that.counter++;
 
-            if(this.isPlaying && this.body.x>-30) this.body.x -=3;
+            if(this.isPlaying) this.body.x -=3;
         },
 
         addObstacleItem: function(){
             // this.body.y = Math.round(Math.random()*500);
             // this.body.y = (yPos+11) + (Math.random()*height) - 11;
-            this.body.x = fullWidth;
+            // this.body.x = fullWidth;
         },
 
         powerUpToken: function(){
-            this.body.x = -30;
+            // this.body.x = -30;
         },
 
         endGame: function(){
